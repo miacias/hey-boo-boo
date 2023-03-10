7c6ea3fe516518cdf37c8296698c3fc4ab943f8f
Mia Ciasullo <miaciasullo@gmail.com>
2023-03-09 20:25:57 -0500

    SOLVED: added requirements and middleware to server.js

--- config/connection.js
+++ config/connection.js
@@ -7,16 +7,16 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: "localhost",
-        dialect: "mysql",
-        port: 3306,
-      }
-    );
-    
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+
 //cookies
 const sess = {
   secret: process.env.SESSION_SECRET,
--- server.js
+++ server.js
@@ -1,28 +1,38 @@
-const path = require("path");
-const express = require("express");
-require("dotenv").config();
+// express.js
+const express = require('express');
+const session = require('express-session');
+const config = require("./config/connection.js");
 
-// import express session and store sequelize connection to new class type
-const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-// express session and handlebars
-const exphbs = require("express-handlebars");
+// express routes
+const path = require('path'); 
+const router = require('./controllers');
 
-// const router = require("./controllers");
-const config = require("./config/connection");
-// const helpers = require('./utils/helpers';)
+// handlebars.js
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+
+// environment variables
+require('dotenv').config();
+
+// sequelize
+const sequelize = require('./config/connection.js');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const hbs = exphbs.create();
 const PORT = process.env.PORT || 3001;
+// enables Handlebars helpers
+const hbs = exphbs.create({ helpers });
+
+app.use(session(config.sess));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
-app.use(router);
-app.use(session(config.sess));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(router);
 
 config.sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(` Now Listening on port ${PORT}`));
