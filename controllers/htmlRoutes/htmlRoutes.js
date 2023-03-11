f0be86641a21693382780058aa787e66c095b4c8
miacias <miaciasullo@gmail.com>
2023-03-11 13:34:48 -0500

    building / logged in route to render html

--- controllers/htmlRoutes/htmlRoutes.js
+++ controllers/htmlRoutes/htmlRoutes.js
@@ -12,6 +12,19 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
-
+// get logged in home page with all picnics for user
+router.get('/', async (req, res) => {
+    console.log("GET: logged in home", req.session.user_id, req.session.logged_in);
+    try {
+        const allMyPicnics = await Picnic.findAll({
+            attributes: ['id', ],
+            where: { userId: req.session.user_id },
+            order: [['start_time', 'DESC']],
+        })
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
