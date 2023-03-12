91bd05cf1b3660d6fca82cf5efaa230d08cae100
ragingpenguin <eflinjh@gmail.com>
2023-03-12 06:03:51 -0400

    added another potential good idea

--- GoogAPI/scratchpad.js
+++ GoogAPI/scratchpad.js
@@ -71,7 +71,62 @@ calendar.freebusy.query({
 //     "auth_provider_cert_url": "xxxxxxxxxxxxxxxxx",
 //     "client_cert_url": "xxxxxxxxxxxxxxxxx"
 //   }
+
+// this code might work here tool
+// const GoogleAuth = require('google-auth-library');
+
+// function authorize() {
+//     return new Promise(resolve => {
+//         const authFactory = new GoogleAuth();
+//         const jwtClient = new authFactory.JWT(
+//             process.env.GOOGLE_CLIENT_EMAIL, // defined in Heroku
+//             null,
+//             process.env.GOOGLE_PRIVATE_KEY, // defined in Heroku
+//             ['https://www.googleapis.com/auth/calendar']
+//         );
+
+//         jwtClient.authorize(() => resolve(jwtClient));
+//     });
+// }
+// const fs = require('fs');
+
+// async function createJSONFile() {
+
+//   //Replace '\' in environment variable .env file before JSON.stringify() 
+//   //so that stringify does not turn it into '\\'
+//   //https://stackoverflow.com/a/36439803/13604562
+//   jsonFile.private_key = process.env.GCS_JSON_private_key.replace(/\\n/g, '\n');
+
+//   let data = JSON.stringify(jsonFile);
+//   //CHECK IF JSON KEYFILE FOR GCS EXISTS. IF NOT, CREATES FILE
+//   if (!fs.existsSync(`./${process.env.GCS_KEYFILE}`)) {
+//     await fs.writeFile(`./${process.env.GCS_KEYFILE}`, data, function (err) {
+//       if (err) {
+//         return res.status(400).json(err);
+//       }
+//     });
+//   }
+// }
+
+// //JSON file template
+// let jsonFile = {
+//   type: `${process.env.GCS_JSON_type}`,
+//   project_id: `${process.env.GCS_JSON_project_id}`,
+//   private_key_id: `${process.env.GCS_JSON_private_key_id}`,
+//   private_key: `${process.env.GCS_JSON_private_key}`,
+//   client_email: `${process.env.GCS_JSON_client_email}`,
+//   client_id: `${process.env.GCS_JSON_client_id}`,
+//   auth_uri: `${process.env.GCS_JSON_auth_uri}`,
+//   token_uri: `${process.env.GCS_JSON_token_uri}`,
+//   auth_provider_x509_cert_url: `${process.env.GCS_JSON_auth_provider_x509_cert_url}`,
+//   client_x509_cert_url: `${process.env.GCS_JSON_client_x509_cert_url}`,
+// };
+
+// createJSONFile();
 //  ref: https://dev.to/sylviapap/setting-heroku-config-vars-with-google-cloud-json-file-on-rails-4dnf
+
+
+// 
 
 const Translate = require('@google-cloud/translate');
 const projectId = 'your project id here';
