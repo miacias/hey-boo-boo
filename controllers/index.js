bac543f6c83f810940e715e5e7e3646347b05e83
miacias <miaciasullo@gmail.com>
2023-03-12 14:25:00 -0400

    completed last merge conflict of routes. added descriptive comments

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,4 @@
 const router = require('express').Router();
-<<<<<<< HEAD
-// const htmlRoutes = require('./htmlRoutes');
-const apiRoutes = require('./apiRoutes');
-
-// router.use('/', htmlRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
-=======
 const htmlRoutes = require('./htmlRoutes');
 const apiRoutes = require('./apiRoutes');
 
@@ -15,4 +6,3 @@ router.use('/', htmlRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
->>>>>>> 7ab39b1009eebfbb534ed7e04b3a4ec6a4b7d4ca
