37ad127134156e7c90de3c87a80ed81b313ab610
skullkid4200 <jackeinhorn123@gmail.com>
2023-03-14 15:25:28 -0400

    started working front-end on login functionality

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
-const signupForm = document.querySelector('.signup-form');
-const loginForm = document.querySelector('.login-form');
+const signupForm = document.querySelector('#signup-form');
+const loginForm = document.querySelector('#login-form');
 
 async function handleSignup(event) {
   event.preventDefault();
@@ -42,7 +42,7 @@ async function handleLogin(event) {
     password: passwordValue,
   };
 
-  const response = await fetch('/api/login', {
+  const response = await fetch('/api/users/login', {
     body: JSON.stringify(loginUser),
     method: 'POST',
     headers: {
