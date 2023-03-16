c7761e44525f23a37a611975ac5c1227a99eacc8
ragingpenguin <eflinjh@gmail.com>
2023-03-16 18:52:45 -0400

    can now add picnics from database to goog calendar

--- GoogAPI/event.js
+++ GoogAPI/event.js
@@ -4,13 +4,13 @@ const { User, Picnic, Food, PicnicUser, FoodPicnicUser } = require('../models');
 const sequelize = require('../config/connection')
 
 class PicnicEvent {
-    constructor(summary, location, description, start, end,attendees){
+    constructor(summary, location, description, start, end,/*attendees*/){
     this.summary = summary;
     this.location = location;
     this.description = description;
     this.start = start;
     this.end = end
-    this.attendees = attendees
+    // this.attendees = attendees
    }
 
 }
@@ -29,7 +29,7 @@ const event = {
 },
 
 }
-// console.log(event)
+ console.log(event)
 function buildParams(event){
     const params =[]
 for (const key in event){
@@ -47,7 +47,7 @@ const myPicnics =  Picnic.findAll({
         through: PicnicUser,
     },
     where: {
-        id: /* req.session.user_id */  3
+        id: /* this needs to take a dynamic ID... req.session.user_id */  3
     },
     order: [['start_time', 'DESC']],
     
@@ -55,13 +55,13 @@ const myPicnics =  Picnic.findAll({
 
 // this function is no longer needed
 
-// const SQLDateTime = '2023-03-14T16:04:56.000Z'
-// function convertDateTime(SQLDateTime){
-//     const newDateTime = SQLDateTime.slice(0,19);
-//     const googleDateTime = newDateTime.concat('-05:00')
-//     return googleDateTime
-// }
-// const googleDateTime =convertDateTime(SQLDateTime)
+const SQLDateTime = '2023-03-14T16:04:56.000Z'
+function convertDateTime(SQLDateTime){
+    const newDateTime = SQLDateTime.slice(0,19);
+    const googleDateTime = newDateTime.concat('-04:00')
+    return googleDateTime
+}
+const googleDateTime =convertDateTime(SQLDateTime)
 // console.log(googleDateTime)
 
 //  used Date to set new time to two hours later.
@@ -70,8 +70,8 @@ function createEndTime(startTime){
     dateTime.setHours(dateTime.getHours() + 2);
     return dateTime.toISOString();
 }
-console.log(myPicnics)
-// console.log(createEndTime(googleDateTime))
+// console.log(myPicnics)
+ console.log(createEndTime(googleDateTime))
 // myPicnics returns a promise, use a .then() to render the resulting object, and assign its properties to variables to be passed into
 // the build params function and fed to a new picnic event object to be sent to calendar api.
 // IF MYPICNICS IS NOT A PROMISE USE THIS
@@ -101,35 +101,40 @@ console.log(myPicnics)
 
 
 // USE BELOW IF MYPICNICS IS A PENDING PROMISE
-// myPicnics.then((data)=>{
-//     const thisPicnic = data[0].dataValues
-//     const summary = thisPicnic.event_name;
-//     const location = thisPicnic.address;
-//     const description = summary
-//     const start = `{'dateTime':${new Date(thisPicnic.start_time).toISOString()},'timeZone': 'America/New_York',}`
-//     const end =`{'dateTime':${createEndTime(thisPicnic.start_time)},'timeZone': 'America/New_York',}` 
-//     const attending =thisPicnic.users
-//     const newEvent = {summary, location, summary, start, end}
-//     const attendees = []
-//     // console.log(attending)
-//     for (invited of attending){
-//         // const first =invited.dataValues.first_name;
-//         // const last = invited.dataValues.last_name;
-//         // const attendee = first.concat(` ${last}`)
-//         const email = invited.dataValues.email
-//         attendees.push(`{'email': '${email}'}`)
-//     }
-//     // console.log(attendees)
-//      console.log(new PicnicEvent(summary, location, description, start, end, attendees))
-//      return new PicnicEvent(summary, location, description, start, end, attendees)
-// })
+myPicnics.then((data)=>{
+    const thisPicnic = data[0].dataValues
+    const summary = thisPicnic.event_name;
+    const location = thisPicnic.address;
+    const description = summary
+    const start = {
+        'dateTime':`${convertDateTime(new Date(thisPicnic.start_time).toISOString())}`,
+        'timeZone': 'America/New_York'
+      }
+    const end ={
+        'dateTime':`${convertDateTime(createEndTime(thisPicnic.start_time))}`,
+        'timeZone': 'America/New_York'} 
+    // const start = `{'dateTime':${convertDateTime(new Date(thisPicnic.start_time).toISOString())},'timeZone': 'America/New_York',}`
+    // const end =`{'dateTime':${convertDateTime(createEndTime(thisPicnic.start_time))},'timeZone': 'America/New_York',}` 
+    const attending =thisPicnic.users
+    const newEvent = {summary, location, summary, start, end}
+    const attendees = []
+    // console.log(attending)
+    for (invited of attending){
+        // const first =invited.dataValues.first_name;
+        // const last = invited.dataValues.last_name;
+        // const attendee = first.concat(` ${last}`)
+        const email = invited.dataValues.email
+        attendees.push(`{'email': '${email}'}`)
+    }
+    // console.log(attendees)
+     console.log(new PicnicEvent(summary, location, description, start, end, attendees))
+     return new PicnicEvent(summary, location, description, start, end, attendees)
+})
 
 
 
 
 
-module.exports= {PicnicEvent, buildParams,createEndTime}
+module.exports= {PicnicEvent, buildParams,createEndTime, convertDateTime}
 
 
-
-    
