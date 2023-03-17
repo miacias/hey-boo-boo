f1ee8e403addd6169ef55baf6e1b65e4010f0d84
ragingpenguin <eflinjh@gmail.com>
2023-03-17 00:52:33 -0400

    moved the DB query off the router, exported it as an async function called googEventCreator. router is much easier to read now

--- GoogAPI/event.js
+++ GoogAPI/event.js
@@ -1,137 +1,84 @@
 // This file will create a class for Calendar events for easy insertion into DB and API requests
-const Sequelize = require('sequelize')
-const { User, Picnic, Food, PicnicUser, FoodPicnicUser } = require('../models');
-const sequelize = require('../config/connection')
+const Sequelize = require("sequelize");
+const { User, Picnic, Food, PicnicUser, FoodPicnicUser } = require("../models");
+const sequelize = require("../config/connection");
+
 
 class PicnicEvent {
-    constructor(summary, location, description, start, end,/*attendees*/){
+  constructor(summary, location, description, start, end, attendees) {
     this.summary = summary;
     this.location = location;
     this.description = description;
     this.start = start;
-    this.end = end
-    //this.attendees = attendees
-   }
-
+    this.end = end;
+    this.attendees = attendees
+  }
 }
 
-// const event = {
-// 'summary': 'Google I/O 2015',
-// 'location': '800 Howard St., San Francisco, CA 94103',
-// 'description': 'A chance to hear more about Google\'s developer products.',
-// 'start': {
-//   'dateTime': '2023-05-28T09:00:00-07:00',
-//   'timeZone': 'America/Los_Angeles',
-// },
-// 'end': {
-//   'dateTime': '2023-05-28T17:00:00-07:00',
-//   'timeZone': 'America/Los_Angeles',
-// },
-
-// }
-//  console.log(event)
-function buildParams(event){
-    const params =[]
-for (const key in event){
-    params.push(event[key])
-
+function convertDateTime(SQLDateTime) {
+  const newDateTime = SQLDateTime.slice(0, 19);
+  const googleDateTime = newDateTime.concat("-04:00");
+  return googleDateTime;
 }
-return params
-}
-// const picnic = new PicnicEvent(...buildParams(event))
-// console.log(picnic)
-
-const myPicnics =  Picnic.findAll({
-    include: {
-        model: User,
-        through: PicnicUser,
-    },
-    where: {
-        id: /* this needs to take a dynamic ID... req.session.user_id */  3
-    },
-    order: [['start_time', 'DESC']],
-    
-})
-
-function convertDateTime(SQLDateTime){
-    const newDateTime = SQLDateTime.slice(0,19);
-    const googleDateTime = newDateTime.concat('-04:00')
-    return googleDateTime
-}
-// const googleDateTime =convertDateTime(SQLDateTime)
-
 
 //  used Date to set new time to two hours later.
-function createEndTime(startTime){
-    const dateTime = new Date(startTime);
-    dateTime.setHours(dateTime.getHours() + 2);
-    return dateTime.toISOString();
+function createEndTime(startTime) {
+  const dateTime = new Date(startTime);
+  dateTime.setHours(dateTime.getHours() + 2);
+  return dateTime.toISOString();
 }
-// console.log(myPicnics)
-//  console.log(createEndTime(googleDateTime))
-// myPicnics returns a promise, use a .then() to render the resulting object, and assign its properties to variables to be passed into
-// the build params function and fed to a new picnic event object to be sent to calendar api.
-// IF MYPICNICS IS NOT A PROMISE USE THIS
-
-    // const thisPicnic = myPicnics[0].dataValues
-    // const summary = thisPicnic.event_name;
-    // const location = thisPicnic.address;
-    // const description = summary
-    // const start = `{'dateTime':${new Date(thisPicnic.start_time).toISOString()},'timeZone': 'America/New_York',}`
-    // const end =`{'dateTime':${createEndTime(thisPicnic.start_time)},'timeZone': 'America/New_York',}` 
-    // const attending =thisPicnic.users
-    // const newEvent = {summary, location, summary, start, end}
-    // const attendees = []
-    // // console.log(attending)
-    // for (invited of attending){
-    //     // const first =invited.dataValues.first_name;
-    //     // const last = invited.dataValues.last_name;
-    //     // const attendee = first.concat(` ${last}`)
-    //     const email = invited.dataValues.email
-    //     attendees.push(`{'email': '${email}'}`)
-    // }
-    // // console.log(attendees)
-    // return new PicnicEvent(summary, location, description, start, end, attendees)
 
 
+async function googEventCreator(ID) {
+  const myPicnics = Picnic.findAll({
+    include: {
+      model: User,
+      through: PicnicUser,
+    },
+    where: {
+      id: ID,
+    },
+    order: [["start_time", "DESC"]],
+  });
 
-
-
-// USE BELOW IF MYPICNICS IS A PENDING PROMISE
-myPicnics.then((data)=>{
-    const thisPicnic = data[0].dataValues
+  const event = myPicnics.then((data) => {
+    const thisPicnic = data[0].dataValues;
     const summary = thisPicnic.event_name;
     const location = thisPicnic.address;
-    const description = summary
+    const description = summary;
     const start = {
-        'dateTime':`${convertDateTime(new Date(thisPicnic.start_time).toISOString())}`,
-        'timeZone': 'America/New_York'
-      }
-    const end ={
-        'dateTime':`${convertDateTime(createEndTime(thisPicnic.start_time))}`,
-        'timeZone': 'America/New_York'} 
-    // const start = `{'dateTime':${convertDateTime(new Date(thisPicnic.start_time).toISOString())},'timeZone': 'America/New_York',}`
-    // const end =`{'dateTime':${convertDateTime(createEndTime(thisPicnic.start_time))},'timeZone': 'America/New_York',}` 
-    const attending =thisPicnic.users
-    const newEvent = {summary, location, summary, start, end}
-    const attendees = []
+      dateTime: `${convertDateTime(
+        new Date(thisPicnic.start_time).toISOString()
+      )}`,
+      timeZone: "America/New_York",
+    };
+    const end = {
+      dateTime: `${convertDateTime(createEndTime(thisPicnic.start_time))}`,
+      timeZone: "America/New_York",
+    };
+    const attending = thisPicnic.users;
+    const attendees = [];
     // console.log(attending)
-    for (invited of attending){
-        // const first =invited.dataValues.first_name;
-        // const last = invited.dataValues.last_name;
-        // const attendee = first.concat(` ${last}`)
-        const email = invited.dataValues.email
-        attendees.push(`{'email': '${email}'}`)
+    for (invited of attending) {
+      // const first =invited.dataValues.first_name;
+      // const last = invited.dataValues.last_name;
+      // const attendee = first.concat(` ${last}`)
+      const email = invited.dataValues.email;
+      attendees.push({'email': `${email}`});
     }
-    // console.log(attendees)
-     console.log(new PicnicEvent(summary, location, description, start, end, attendees))
-     return new PicnicEvent(summary, location, description, start, end, attendees)
-})
-
-
-
-
-
-module.exports= {PicnicEvent, buildParams,createEndTime, convertDateTime}
-
-
+    const event = new PicnicEvent(
+        summary,
+        location,
+        description,
+        start,
+        end,
+        attendees
+        );
+        return event;
+    })
+    return event;
+}
+module.exports = {
+  PicnicEvent,
+  googEventCreator,
+};
--- controllers/apiRoutes/googleroutes.js
+++ controllers/apiRoutes/googleroutes.js
@@ -1,19 +1,19 @@
 const router = require("express").Router();
 const { google } = require("googleapis");
-const oAuth2Client = require('../../GoogAPI/oauth'); 
-const {PicnicEvent, buildParams,createEndTime,convertDateTime} = require('../../GoogAPI/event.js')
-const { User, Picnic, Food, PicnicUser, FoodPicnicUser } = require('../../models');
-
-
-router.get('/newtab', async (req, res) =>{
-  res.send('<html><body><a href="http://localhost:3001/api/goog/token" target="_blank">add to calendar</a></body></html>');
-});
+const oAuth2Client = require("../../GoogAPI/oauth");
+const { PicnicEvent, googEventCreator } = require("../../GoogAPI/event.js");
+const {
+  User,
+  Picnic,
+  Food,
+  PicnicUser,
+  FoodPicnicUser,
+} = require("../../models");
 
 //  this route creates url for google auth pages
 router.get("/token/:id", async (req, res) => {
   // get a token from the google
-  // console.log(req.body)
-  console.log(req.params.id)
+  console.log(req.params.id);
 
   // generate a url that asks permissions for Blogger and Google Calendar scopes
   const scopes = ["https://www.googleapis.com/auth/calendar"];
@@ -21,141 +21,45 @@ router.get("/token/:id", async (req, res) => {
   const url = oAuth2Client.generateAuthUrl({
     // 'online' (default) or 'offline' (gets refresh_token)
     access_type: "offline",
-    // state: 3,
-    state:`${req.params.id}`,
+
+    state: `${req.params.id}`,
 
     // If you only need one scope you can pass it as a string
     scope: scopes,
   });
-  // console.log(url);
-  
-  res.status(302).redirect(url) ;
+
+  res.status(302).redirect(url);
 });
 
 router.get("/callback", async (req, res) => {
-  // console.log(req)
-  console.log(req.query.state)
+  console.log(req.query.state);
   const ID = req.query.state;
-  
-  const { tokens } =  await oAuth2Client.getToken(req.query.code);
+
+  const { tokens } = await oAuth2Client.getToken(req.query.code);
   oAuth2Client.setCredentials(tokens);
   console.log(tokens);
   const calendar = google.calendar({ version: "v3", auth: oAuth2Client });
-  const myPicnics =  Picnic.findAll({
-    include: {
-        model: User,
-        through: PicnicUser,
-    },
-    where: {
-        id: ID
-    },
-    order: [['start_time', 'DESC']],
-    
-})
- 
-const event1 = myPicnics.then((data)=>{
-  const thisPicnic = data[0].dataValues
-  const summary = thisPicnic.event_name;
-  const location = thisPicnic.address;
-  const description = summary
-  const start = {
-    'dateTime':`${convertDateTime(new Date(thisPicnic.start_time).toISOString())}`,
-    'timeZone': 'America/New_York'
-  }
-  const end ={
-    'dateTime':`${convertDateTime(createEndTime(thisPicnic.start_time))}`,
-    'timeZone': 'America/New_York'} 
-  const attending =thisPicnic.users
-  const attendees = []
-  // console.log(attending)
-  for (invited of attending){
-    // const first =invited.dataValues.first_name;
-    // const last = invited.dataValues.last_name;
-    // const attendee = first.concat(` ${last}`)
-    const email = invited.dataValues.email
-    attendees.push(`{'email': '${email}'}`)
-  }
-  const event = new PicnicEvent(summary, location,description, start, end /*attendees*/)
+  const event = await googEventCreator(ID);
+  console.log("----------- ROUTER\n");
+  console.log(event);
 
   calendar.events.insert(
     {
       auth: oAuth2Client,
       calendarId: "primary",
-      resource:event ,
+      resource: event,
     },
     function (err, event) {
       if (err) {
         console.log(
           "There was an error contacting the Calendar service: " + err
-          );
-          return;
-        }
-        console.log("Event created: %s", event.data);
-        res.status(302).redirect(event.data.htmlLink);
-        
+        );
+        return;
       }
-      );
-    })
-  });
-  
-  module.exports = router;
-  
-    // const event3 = 
-  //   {
-  //         'summary': 'Google I/O 2015',
-  //         'location': '800 Howard St., San Francisco, CA 94103',
-  //         'description': 'A chance to hear more about Google\'s developer products.',
-  //         'start': {
-  //             'dateTime': '2023-03-14T23:04:56.000Z',
-  //             'timeZone': 'America/New_York',
-  //           },
-  //           'end': {
-  //               'dateTime': '2023-03-14T23:04:56.000Z',
-  //               'timeZone': 'America/New_York',
-  //             },
-  //             'recurrence': [
-  //                 'RRULE:FREQ=DAILY;COUNT=2'
-  //               ],
-  //               'attendees': [
-  //                   {'email': 'lpage@example.com'},
-  //       {'email': 'sbrin@example.com'},
-  //     ],
-  //     'reminders': {
-  //         'useDefault': false,
-  //         'overrides': [
-  //             {'method': 'email', 'minutes': 24 * 60},
-  //             {'method': 'popup', 'minutes': 10},
-  //           ],
-  //      },
-  //    };
-  //    console.log(event3)
-    
-  //   const event =
-  //    {
-  //    summary: 'Food Time',
-  //    location: '3451 Walnut Street Philadelphia, PA 19104',
-  //    description: 'Food Time',
-  //    start: {
-  //          'dateTime': '2023-05-28T09:00:00-04:00',
-  //          'timeZone': 'America/New_York',
-  //        },
-  //    end: {
-  //          'dateTime': '2023-05-28T17:00:00-04:00',
-  //          'timeZone': 'America/New_York',
-  //        }
-  //  }
-  //   {
-  //    'summary': 'Google I/O 2015',
-  //    'location': '800 Howard St., San Francisco, CA 94103',
-  //    'description': 'A chance to hear more about Google\'s developer products.',
-  //    'start': {
-  //      'dateTime': '2023-05-28T09:00:00-07:00',
-  //      'timeZone': 'America/New_York',
-  //    },
-  //    'end': {
-  //      'dateTime': '2023-05-28T17:00:00-07:00',
-  //      'timeZone': 'America/New_York',
-  //    }
-  //  }
-  
-  //   console.log(event)
+      console.log("Event created: %s", event.data);
+      res.status(302).redirect(event.data.htmlLink);
+    }
+  );
+});
+
+module.exports = router;
