34f015518f2d64d40e1bb0d2fc3277d6001d3669
Codename O'Connell <oconnellcoder@gmail.com>
2023-03-16 21:53:21 -0400

    changed route to get all foods

--- controllers/htmlRoutes/picnicRoutes.js
+++ controllers/htmlRoutes/picnicRoutes.js
@@ -1,55 +1,56 @@
-const router = require('express').Router();
-const { User, Picnic, Food , PicnicUser, FoodPicnicUser} = require('../../models');
+const router = require("express").Router();
+const {
+  User,
+  Picnic,
+  Food,
+  PicnicUser,
+  FoodPicnicUser,
+} = require("../../models");
 // const withAuth = require('../../utils/auth.js');
 
 
-// //view specific picnic by id
-// router.get('/:id', withAuth, async (req, res) => {
-//     try {
-//         const searchedPicnic = await Picnic.findOne({
-//          where: {
-//           id: req.params.id
-//          },
-//           include: [User, Food]
-//        });
-//        if (!searchedPicnic) {
-//         return res.status(404).json({
-//           "message": "No picnic with that id was found."
-//         });
-//        }
-//        return res.json(searchData);
-//     } catch(err) {
-//         return res.status(500).json(err);
-//     }
-// });
-
-
 // find all users attending one picnic
-// NEED TO MAKE A SECOND QUERY TO FIND USER DATA FOR CREATOR_ROLE
 // need to add food-related stuff
-router.get('/:id', /*withAuth,*/ async (req, res) => {
-    console.log(req.params.id);
+router.get("/:id", /*withAuth,*/ async (req, res) => {
     try {
-        const allAttendees = await Picnic.findOne({
-            where: {id: req.params.id},
-            include: {
-                model: User,
-                through: PicnicUser,
-            },
+        const allFoods = await Food.findAll({
+            attributes: {exclude: ["id"]},
+            include: [
+                {
+                    model: PicnicUser, attributes: {exclude: ["picnicUserId"]},
+                    where: {picnicId: req.params.id},
+                    include: [
+                        {
+                            model: User
+                        },
+                        {
+                            model: Picnic
+                        },
+                    ]
+                }
+            ]
         });
-        const creator = await allAttendees.dataValues.creator_role;
-        const host = await User.findOne({
-            where: {id: creator}
-        });
-        // console.log(host)
-        // console.log(allAttendees)
-        // res.send({allAttendees, host}); //for insomnia testing
-        res.render("picnicview", {allAttendees, host});
-    } catch (err) {
-        console.error(err);
-        res.status(500).json(err);
-    }
-});
 
+
+        // const allAttendees = await Picnic.findOne({
+        //     where: {id: req.params.id},
+        //     include: User
+        // });
+
+        // const creator = await allFoods.dataValues.picnicUsers.dataValues.picnic.creator_role;
+        // const host = await User.findOne({
+        //     where: {id: creator}
+        // });
+
+        // res.send({ allFoods }); //for insomnia testing
+
+      res.render("picnicview", { allFoods });
+
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  }
+);
 
 module.exports = router;
