c8eceff54a27619f5f466057dd70d5741c351ab3
miacias <miaciasullo@gmail.com>
2023-03-18 15:44:40 -0400

    re-enabled google maps autocomplete. corrected popper to close when clicking on webpage

--- public/js/myPicnics.js
+++ public/js/myPicnics.js
@@ -1,11 +1,12 @@
-
 // all invite buttons
 const inviteBtn = document.querySelectorAll(".invite-link");
 
-
+// enables Popper-Bootstrap popper
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-var popoverList =  popoverTriggerList.map(function (element) {
-    return new bootstrap.Popover(element);
+var popoverList = popoverTriggerList.map(function (element) {
+    return new bootstrap.Popover(element, {
+        trigger: 'focus'
+      });
 });
 
 // copies invite code to clipboard, displays in popover
@@ -15,7 +16,6 @@ const copyLink = (event) => {
     const inviteLink = event.currentTarget.getAttribute('id');
     // Copy the text inside the text field
     navigator.clipboard.writeText(`${window.location.origin}/api/new-picnic/join/${inviteLink}`);
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
 };
 
 inviteBtn.forEach(button => button.addEventListener('click', copyLink));
